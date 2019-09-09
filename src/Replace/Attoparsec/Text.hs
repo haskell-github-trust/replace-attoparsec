3c4ba39d9cd9d32da3336a652564905e84fbd9d6
James Brock <jamesbrock@gmail.com>
2019-09-09 23:06:30 +0900

    haddock link typo

--- src/Replace/Attoparsec/Text.hs
+++ src/Replace/Attoparsec/Text.hs
@@ -168,7 +168,7 @@ findAll sep = (fmap.fmap) (second fst) $ sepCap (match sep)
 --
 -- If you want access to the matched string in the @editor@ function,
 -- then combine the pattern parser @sep@
--- with 'Data.AttoParsec.Text.match'. This will effectively change
+-- with 'Data.Attoparsec.Text.match'. This will effectively change
 -- the type of the @editor@ function to @(s,a) -> s@.
 --
 -- This allows us to write an @editor@ function which can choose to not
